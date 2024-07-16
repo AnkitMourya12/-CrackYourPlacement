713862199be3d5ceedc286b5ab01cc600e543522
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2024-07-16 07:14:50 +0530

    Arsh-dsa-sheet array question

---
+++ 1.Two Sum.cpp
@@ -0,0 +1,18 @@
+unordered_map<int,int>mp;
+    int sum=0;
+
+
+    for(int i=0;i<nums.size();i++){
+        int diff=target-nums[i];
+        
+        if(mp.find(diff)!=mp.end()){
+            return {mp[diff],i};
+        }
+        mp[nums[i]]=i;
+    
+     }
+
+    return {};
+
+
+    }
