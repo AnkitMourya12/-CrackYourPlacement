59d9a8cbe5ffb2160a3dec87f4e94182ee16d654
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2024-10-29 16:17:39 +0530

    Create Quick Sort on Linked List.cpp

---
+++ Quick Sort on Linked List.cpp
@@ -0,0 +1,24 @@
+#include<algorithm>
+class Solution {
+  public:
+    struct Node* quickSort(struct Node* head) {
+        // code here
+        if(!head)
+        return head;
+        int i=0;
+        vector<int> arr;
+        Node* temp=head;
+        while(temp!=NULL){
+            arr.push_back(temp->data);
+            temp=temp->next;
+        }
+        sort(arr.begin(),arr.end());
+        temp=head;
+        while(temp!=NULL){
+            temp->data=arr[i];
+            i++;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
