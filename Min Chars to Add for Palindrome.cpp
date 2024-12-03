5d81181ad36a85f9fbc6cc8bbe3a9a1b21460b08
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2024-12-03 19:02:26 +0530

    Create Min Chars to Add for Palindrome.cpp

---
+++ Min Chars to Add for Palindrome.cpp
@@ -0,0 +1,12 @@
+string rev = s;
+        reverse(rev.begin(),rev.end());
+        string combine = s + '#'+rev;
+        vector<int>lps(combine.size(),0);
+        int i = 0, j = 1;
+        while(j<combine.size()){
+            if(combine[j] == combine[i])lps[j++] = ++i;
+            else if(i) i =lps[i-1];
+            else j++;
+        }
+        string temp = s.substr(i);
+        return temp.size();
