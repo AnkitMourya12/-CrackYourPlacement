23485cd3bf628b733e6b41609954c9253b1443a6
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2025-12-08 23:19:34 +0530

    Time: 0 ms (100%), Space: 7.9 MB (55.75%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int countTriples(int n) {
-        vector<bool> isSquare(n * n + 1, false);
-        for (int i = 1; i <= n; i++) {
-            isSquare[i * i] = true;
-        }
-        
         int count = 0;
-        for (int a = 1; a <= n; a++) {
-            for (int b = 1; b <= n; b++) {
-                int sum = a * a + b * b;
-                if (sum <= n * n && isSquare[sum])
-                    count++;
+        
+        // Iterate through all possible values for the hypotenuse 'c'
+        for (int c = 1; c <= n; ++c) {
+            int target = c * c;
+            int left = 1;
+            int right = c - 1;
+            
+            // Two pointers approach to find a^2 + b^2 = c^2
+            while (left < right) {
+                int sum = left * left + right * right;
+                
+                if (sum == target) {
+                    count += 2; // Add 2 because (a,b,c) and (b,a,c) are distinct
+                    left++;
+                    right--;
+                } 
+                else if (sum < target) {
+                    left++; // Sum is too small, increase the smaller number
+                } 
+                else {
+                    right--; // Sum is too big, decrease the larger number
+                }
             }
         }
+        
         return count;
     }
 };
